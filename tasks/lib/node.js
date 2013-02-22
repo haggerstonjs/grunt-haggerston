773e73c2785eff9a03172c07e25155dace47e811
Matt Sweetman <git@webroo.org>
2013-02-22 14:43:34 +0000

    Moved title into templateData so it's no longer a convention

--- tasks/lib/node.js
+++ tasks/lib/node.js
@@ -19,28 +19,23 @@ module.exports = function(basePath, grunt)
     for (var prop in properties) {
       this[prop] = properties[prop];
     }
+  };
 
-    // TODO: Defaults
-    // TODO: Create this.templateData.htmlTitle based on the title?
-
+  Node.prototype.render = function()
+  {
     for (var fieldName in this.templateData) {
       var data = this.templateData[fieldName];
       if (data.match(/\.md$/)) {
         this.templateData[fieldName] = marked(
-            grunt.file.read(basePath + '/content/' + this.path + data),
-            {
-//            highlight: function(code, lang)
-//            {
+            grunt.file.read(basePath + '/content/' + this.path + data), {
+//            highlight: function(code, lang) {
 //              return hljs.highlight(lang, code).value;
 //            }
             }
         );
       }
     }
-  };
 
-  Node.prototype.render = function()
-  {
     var rendered = swig.compileFile(this.template).render(this.templateData);
     return rendered;
   };
